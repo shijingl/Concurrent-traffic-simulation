c3979ae06722ff947f7614112f5124206d6a2619
Shijing Liu <liushijing123@gmail.com>
2022-04-15 18:45:48 -0400

    finish Task5.a

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -10,6 +10,13 @@ T MessageQueue<T>::receive()
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _cond.wait(uLock, [this] { return !_queue.empty(); });
+
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg;
 }
 
 template <typename T>
